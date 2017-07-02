3293e06730281195805f8f66fd81c25d16d626be
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-07-02 23:37:51 +1200

    Add sampel route for heroku testing

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+const Knex = require('knex')
+const knexConfig = require('../knexfile')[process.env.NODE_ENV || 'development']
+const knex = Knex(knexConfig)
 
 /* GET home page. */
+router.get('/api', function(req, res, next) {
+  knex.select('id', 'fullName', 'age', 'url').from('entries')
+  .then(data => {
+    res.send(data)
+  })
+})
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
 
 
-module.exports = router;
+
+module.exports = router
