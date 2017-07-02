6008da6ff1fd2a45c001bcaf2791d36c66a2bd49
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-07-02 23:45:02 +1200

    change knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -9,36 +9,6 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'postgres://dzketovqqkflox:9da0b1c4d27afa9c7a9a9c05445881f838ad364f566205aab5439abf38f0094e@ec2-23-23-234-118.compute-1.amazonaws.com:5432/d431os35homt6q',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  production: process.env.DATABASE_URL
 
 };
