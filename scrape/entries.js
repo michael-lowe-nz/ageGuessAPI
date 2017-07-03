7f0cc9f71b54d29622135ef3b9248e45c229d99b
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-07-04 00:05:56 +1200

    Add scraping for name age and url

---
+++ scrape/entries.js
@@ -0,0 +1,30 @@
+const $ = require('cheerio')
+const request = require('superagent')
+
+const names = ["Lebron_James", "Richie_McCaw", "John_Key", "Matt_Damon"]
+
+const scrapeUrl = (url) => {
+  request
+    .get(url)
+    .end((err, res) => {
+      if (err) return console.log(err)
+      const html = $.load(res.text)
+      const fullName = html('#firstHeading').text()
+      const age = html('.ForceAgeToShow').text()
+      const url = html('.infobox .image img').attr('src')
+      const data = {
+        fullName,
+        age,
+        url
+      }
+      console.log(data)
+    })
+}
+
+const scrapeByName = (name) => {
+  return scrapeUrl(`https://en.wikipedia.org/wiki/${name}`)
+}
+
+const clooney = 'https://en.wikipedia.org/wiki/George_Clooney'
+
+scrapeByName('Lebron_James')
