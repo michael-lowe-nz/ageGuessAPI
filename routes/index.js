7790922d071097c970c8cbb0cb216ee109cdfa86
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-07-07 19:44:49 +1200

    Remove tests for db

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,9 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
-const Knex = require('knex')
-const knexConfig = require('../knexfile')[process.env.NODE_ENV || 'development']
-const knex = Knex(knexConfig)
+/* GET entries */
+router.get('/', function(req, res) {
+  res.render('index', {title: 'ageGuessAPI'})
+});
 
-/* GET home page. */
-router.get('/api', function(req, res, next) {
-  knex.select('id', 'fullName', 'age', 'url').from('entries')
-  .then(data => {
-    res.send(data)
-  })
-})
-
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
-})
-
-
-
-
-module.exports = router
+module.exports = router;
