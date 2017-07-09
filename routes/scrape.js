b9e60d25f735f389ca9f1de21f69d48cea7cfac7
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-07-09 23:19:40 +1200

    Set up route for scraping wikipedia

--- routes/scrape.js
+++ routes/scrape.js
@@ -14,19 +14,20 @@ const {
 const getPeopleFromWiki = require('./../scrape/getPeopleFromWiki')
 
 /* GET entries */
-router.get('/', function(req, res) {
-  getPeopleFromWiki('https://en.wikipedia.org/wiki/Category:Living_people?from=D')
-    .then(urls => {
-      urls.forEach(url => {
-        getDataFromUrl(url)
-          .then(data => addEntry(data))
-          .catch(error => console.log(error))
-      })
+router.get('/:letter', function(req, res) {
+  getPeopleFromWiki(`https://en.wikipedia.org/wiki/Category:Living_people?from=${req.params.letter}`)
+  .then(urls => {
+    urls.forEach(url => {
+      getDataFromUrl(url)
+      .then(data => addEntry(data))
+      .then(response => console.log(response, "added"))
+      .catch(error => console.log(error))
     })
-    .then(() => res.send('Wikipedia scraped and entered'))
-    .catch(error => {
-      res.send('Error!', error)
-    })
+  })
+  .then(() => res.send('Wikipedia scraped and entered'))
+  .catch(error => {
+    res.send('Error!', error)
+  })
 });
 
 module.exports = router;
