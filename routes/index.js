0932a613a0de50969252db12c0f1996da3ee0384
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-07-18 18:44:29 +1200

    Get index to render out all the people

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const {
+  getAllEntries,
+} = require('./../db/entries')
+
 /* GET entries */
 router.get('/', function(req, res) {
-  res.render('index', {title: 'ageGuessAPI'})
+  getAllEntries()
+  .then(entries => {
+    console.log("entries:", entries)
+    res.render('index',
+    {
+      title: 'ageGuessAPI',
+      myArray: ['yo', 'yoo'],
+      entries: entries
+    })
+  })
+  .catch(error => console.log(error))
 });
 
 module.exports = router;
