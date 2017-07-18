1341b5ea537fbc2acb40a9cb22df58d3456e2486
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-07-18 23:49:16 +1200

    Change test script to test all files

--- test/getPeopleFromWiki.js
+++ test/getPeopleFromWiki.js
@@ -6,3 +6,13 @@ test('can get an array from getPeopleFromWiki with a length greater than 0', t =
     .then(response => t.ok(response.length))
     .catch(error => t.notOk(error, 'should be no error'))
 })
+
+// How should getting people form the 'next' wikis work?
+// Do we gather up all the urls and then getPeopleFromWikiOnAllOfThem?
+
+// Ask for people from a certain letter, e.g 'A'
+// Iterate through A -> Aa -> Ab -> Ac -> Ad -> Ae ...
+// getPeopleFromWiki(...?from=A)
+// getPeopleFromWiki(...?from=Ab)
+// getData from all the people in the array
+// if data is appropriate, store in the DB
