1e8f3e60967d27dec6aad95bbf50f299bbd37b5a
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-07-25 22:53:58 +1200

    Add fb button to login

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser')
 var entries = require('./routes/entries')
 var scrape = require('./routes/scrape')
 var index = require('./routes/index')
+var login = require('./routes/login')
 
 var app = express()
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api/entries', entries)
 app.use('/scrape', scrape)
+app.use('/login', login)
 app.use('/', index)
 
 // catch 404 and forward to error handler
